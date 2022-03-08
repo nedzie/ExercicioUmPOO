bc6ed44ae5913dee6ce0e90d54b215cbb24a9ba0
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 14:45:26 -0300

    Add project files.

---
+++ ExercicioUm.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioUm.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new();
+            Console.WriteLine("Informe o comprimento do retângulo: \n> ");
+            retangulo.comprimento = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a largua do retângulo: \n> ");
+            retangulo.largura = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a altura do retângulo: \n> ");
+            retangulo.altura = decimal.Parse(Console.ReadLine());
+            retangulo.volume = retangulo.CalcularVolume(retangulo.comprimento, retangulo.largura, retangulo.altura);
+
+            Console.WriteLine("Volume do retangulo: " + retangulo.volume.ToString("#.##") + ".");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExercicioUm.ConsoleApp/Retangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioUm.ConsoleApp
+{
+    internal class Retangulo
+    {
+        public decimal comprimento;
+        public decimal largura;
+        public decimal altura;
+        public decimal volume;
+
+        public decimal CalcularVolume(decimal comprimento, decimal largura, decimal altura)
+        {
+            volume = altura * comprimento * largura;
+            return volume;
+        }
+    }
+}
